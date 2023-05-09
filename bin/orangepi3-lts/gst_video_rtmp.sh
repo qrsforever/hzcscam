8f02eaaf231915507805d59bc67fa6791cd2b3fe
Li Dong <erlangai@qq.com>
2023-05-09 11:53:23 +0800

    save

---
+++ bin/orangepi3-lts/gst_video_rtmp.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+CUR_DIR=$(cd $(dirname ${BASH_SOURCE[0]}); pwd)
+
+# http://101.42.139.3:30808/players/rtc_player.html?vhost=seg.300s&ip=192.168.152.185&api=31985&app=live&stream=orangepizero2
+
+source /etc/orangepi-release
+VIDEO_DVICE=/dev/video1
+RTMP_DOMAIN=srs.hzcsdata.com
+
+while (( 1 ))
+do
+    if [[ -e ${VIDEO_DVICE} ]]
+    then
+        netok=$(ping -c 1 -W 2 ${RTMP_DOMAIN} 2>/dev/null | grep -o "received")
+        if [[ x${netok} != x ]]
+        then
+            gst-launch-1.0 v4l2src device=${VIDEO_DVICE} io-mode=4 ! \
+                video/x-raw, width=640, height=480, framerate=25/1 ! videoconvert ! \
+                x264enc bframes=0 speed-preset=veryfast key-int-max=30 ! \
+                flvmux streamable=true ! rtmpsink location=rtmp://${RTMP_DOMAIN}/live/${BOARD}?vhost=seg.300s
+        fi
+    fi
+    sleep 5
+done
---
+++ scripts/install_gst_service.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+CUR_DIR=$(cd $(dirname ${BASH_SOURCE[0]}); pwd)
+TOP_DIR=$(dirname $CUR_DIR)
+
+SERVICE=campi_gst.service
+
+USER=root
+ROOT_DIR=/campi
+
+BOARD=$(cat /etc/orangepi-release | grep BOARD= | cut -d= -f2)
+SRC_DIR=${TOP_DIR}/etc/${BOARD}
+DST_DIR=/etc/systemd/system/
+
+cat > ${SRC_DIR}/$SERVICE <<EOF
+[Unit]
+    Description=System Gstreamer
+    Documentation=http://campi.hzcsai.com
+
+[Service]
+    Type=simple
+    User=$USER
+    Group=$USER
+    UMask=0000
+    WorkingDirectory=${ROOT_DIR}
+    Environment="PYTHONPATH=${ROOT_DIR}"
+    ExecStart=bash ${TOP_DIR}/bin/${BOARD}/gst_video_rtmp.sh
+
+[Install]
+    WantedBy=multi-user.target
+EOF
+
+systemctl stop $SERVICE 2>&1 /dev/null
+cp ${SRC_DIR}/$SERVICE $DST_DIR
+systemctl daemon-reload
+systemctl enable $SERVICE
+systemctl restart $SERVICE
+systemctl status $SERVICE
+journalctl -u $SERVICE --no-pager -n 10
+echo "-------------------------------"
+echo ""
+echo "journalctl -u $SERVICE -f -n 100"
+echo ""
+echo "-------------------------------"
