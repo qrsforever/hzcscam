0b3550cd73d2a6da046774324ef096337bab6b51
Li Dong <erlangai@qq.com>
2023-07-03 11:14:31 +0800

    short press 3s

--- campi/app/emq/sensors.c
+++ campi/app/emq/sensors.c
@@ -415,7 +415,7 @@ void sensor_detect()
             if ((time(0) - btn_onpress_timer) > 2) {
                 syslog(LOG_DEBUG, "press count: %d\n", short_press_count);
                 switch (short_press_count) {
-                    case 2: {
+                    case 3: {
                         syslog(LOG_DEBUG, "start ota auto upgrade!\n");
                         neza_pub("upgrade/auto", "{}");
                         break;
