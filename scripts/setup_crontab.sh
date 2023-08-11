2727abea6b5152f85928058d2156909a61ce57cc
Li Dong <erlangai@qq.com>
2023-08-11 18:20:10 +0800

    save

--- scripts/setup_crontab.sh
+++ scripts/setup_crontab.sh
@@ -13,7 +13,6 @@ cp ${TOP_DIR}/board/${BOARD}/bin/sysled /usr/local/bin/sysled
 chmod +x /usr/local/bin/campi_safe_run.sh
 chmod +x /usr/local/bin/sysled
 
-
 cat > /tmp/crontab <<EOF
 SHELL=/bin/sh
 PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
@@ -24,7 +23,7 @@ PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 47 6	* * 7	root	test -x /usr/sbin/anacron || ( cd / && run-parts --report /etc/cron.weekly )
 52 6	1 * *	root	test -x /usr/sbin/anacron || ( cd / && run-parts --report /etc/cron.monthly )
 
-${M} ${H}	* * *	root    sleep 10 && reboot -f
+${M} ${H}	* * *	root    campi_safe_run.sh
 
 @reboot root campi_safe_run.sh
 EOF
@@ -32,4 +31,4 @@ EOF
 mv /tmp/crontab /etc/crontab
 chmod 644 /etc/crontab
 
-# systemctl status cron.service
+systemctl restart cron.service
